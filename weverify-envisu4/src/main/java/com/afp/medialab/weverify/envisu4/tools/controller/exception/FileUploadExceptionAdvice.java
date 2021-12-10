957e52cb44a33b54db47b8e671f845299f4d03e4
Bertrand Goupil <bertrand.goupil@afp.com>
2021-12-10 17:58:08 +0100

    bug fix: oversize server limit handle exception for file upload

--- weverify-envisu4/src/main/java/com/afp/medialab/weverify/envisu4/tools/controller/exception/FileUploadExceptionAdvice.java
+++ weverify-envisu4/src/main/java/com/afp/medialab/weverify/envisu4/tools/controller/exception/FileUploadExceptionAdvice.java
@@ -1,22 +1,31 @@
 package com.afp.medialab.weverify.envisu4.tools.controller.exception;
 
 import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @ControllerAdvice
 public class FileUploadExceptionAdvice {
 
 	private static Logger Logger = LoggerFactory.getLogger(FileUploadExceptionAdvice.class);
-	
-	@ExceptionHandler({ MaxUploadSizeExceededException.class })
+
+	@ExceptionHandler(SizeLimitExceededException.class)
+	public ResponseEntity<Envisu4ServiceError> handleError1(SizeLimitExceededException ex) {
+
+		Logger.error("server error: " + ServiceErrorCode.IPOL_IMAGE_UPLOAD_EXCEEDED + " - " + ex.getMessage());
+		Envisu4ServiceError ipolServiceError = new Envisu4ServiceError(ServiceErrorCode.IPOL_IMAGE_UPLOAD_EXCEEDED,
+				ex.getMessage());
+		return new ResponseEntity<Envisu4ServiceError>(ipolServiceError, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler({ FileSizeLimitExceededException.class })
 	public ResponseEntity<Envisu4ServiceError> handle(FileSizeLimitExceededException ex) {
-		Logger.error(ServiceErrorCode.IPOL_IMAGE_UPLOAD_EXCEEDED +" - " + ex.getMessage());
+		Logger.error(ServiceErrorCode.IPOL_IMAGE_UPLOAD_EXCEEDED + " - " + ex.getMessage());
 		Envisu4ServiceError ipolServiceError = new Envisu4ServiceError(ServiceErrorCode.IPOL_IMAGE_UPLOAD_EXCEEDED,
 				ex.getMessage());
 		return new ResponseEntity<Envisu4ServiceError>(ipolServiceError, HttpStatus.INTERNAL_SERVER_ERROR);
